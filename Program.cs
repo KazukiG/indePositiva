5da9cae7f9d53d112cc26846982f74efe90b9b66
kazukig <kazukigushiken20@gmail.com>
2021-03-04 18:02:25 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace indePositiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
